f432688a8c2edc87e705455a5047f88570bd73bb
Luis <luisegrezende2005@gmail.com>
2025-08-28 10:48:41 -0300

    mudanças feitas

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -3,12 +3,13 @@ A = float(input("Digite o valor do lado A: "))
 B = float(input("Digite o valor do lado B: "))
 C = float(input("Digite o valor do lado C: "))
 # Verifica se os lados formam um triângulo
-if A < B + C and B < A + C and C < A + B:
-    if A == B == C:
-        print("equilátero")
-    elif A == B or B == C or A == C:
-        print("isósceles")
-    else:
-        print("escaleno")
-else:
+if A + B <= C or A + C <= B or B + C <= A:
     print("não é um triângulo")
+    exit()
+if A == B == C:
+    print("equilátero")
+elif A == B or B == C or A == C:
+    print("isósceles")
+else:
+    print("escaleno")
+# --- IGNORE ---
